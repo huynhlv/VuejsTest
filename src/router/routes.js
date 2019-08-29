d8ede340a22a2253dba4d4618ea7ab524333feee
huynhlv <huynhlevan1997@gmail.com>
2019-08-29 15:39:19 +0700

    change folder router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,10 @@
-import Login from './components/Authen/Login.vue'
-import CampaignManager from './components/DashBoard/CampaignManager'
-import Campaign from './components/CampaignManager/Campaign'
-import AdGroup from './components/CampaignManager/AdGroup'
-import Ads from './components/CampaignManager/Ads'
-import Advertiser from './components/DashBoard/Advertiser'
-import DashBoard from './components/DashBoard/DashBoard'
+import Login from '../components/Authen/Login.vue'
+import CampaignManager from '../components/DashBoard/CampaignManager'
+import Campaign from '../components/CampaignManager/Campaign'
+import AdGroup from '../components/CampaignManager/AdGroup'
+import Ads from '../components/CampaignManager/Ads'
+import Advertiser from '../components/DashBoard/Advertiser'
+import DashBoard from '../components/DashBoard/DashBoard'
 export const routes = [
   { path: '/login', name: 'login', component: Login },
   { path: '/', name: 'dashboard', component: DashBoard, children: [
